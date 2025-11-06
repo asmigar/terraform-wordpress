b50421d5942dbaa975b7cce331d23afdae36ac65
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2025-11-06 12:02:01 +0530

    create rds snapshot at deletion

--- db.tf
+++ db.tf
@@ -1,3 +1,13 @@
+data "aws_db_cluster_snapshot" "wordpress_db_snapshot" {
+  db_cluster_identifier = "aurora-cluster-wordpress" # Replace with your snapshot ARN or identifier
+  most_recent          = true # Set to true to get the latest snapshot
+}
+
+resource "random_string" "snapshot_identifier_suffix" {
+  length           = 4
+  special          = false
+}
+
 resource "aws_rds_cluster" "wordpress" {
   cluster_identifier      = "aurora-cluster-wordpress"
   engine                  = "aurora-mysql"
@@ -9,7 +19,9 @@ resource "aws_rds_cluster" "wordpress" {
   database_name           = "wordpress_db"
   master_username         = "wordpress_user"
   master_password         = "your_strong_password"
-  skip_final_snapshot = true
+  snapshot_identifier = data.aws_db_cluster_snapshot.wordpress_db_snapshot.id
+  skip_final_snapshot = false
+  final_snapshot_identifier = "aurora-cluster-wordpress-${random_string.snapshot_identifier_suffix.id}"
   apply_immediately = true
   preferred_backup_window = "07:00-09:00"
 
